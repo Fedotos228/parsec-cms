1870f5e6f83b18317d77c74e5dfa795eeb1788be
Fedotos228 <tudor.onceanu@gmail.com>
2025-05-27 11:54:26 +0300

    againn

---
+++ dist/src/api/clients-logo/services/clients-logo.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * clients-logo service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::clients-logo.clients-logo');
